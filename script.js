5584f3e69c01d3eec43a61ad7b8b3a39f4657216
Waycool05 <waycool05@hotmail.com>
2025-06-21 17:21:48 -0300

    version3

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 let Variable_Number = 10
 let variable_String = "primer ejercicio"
 let Variable_Boolean = true
+console.log("La variable del Tipo Number es: " + Variable_Number);
+console.log("La variable del Tipo String es: " + variable_String); 
+console.log("La variable del Tipo Boolean es: " + Variable_Boolean);
